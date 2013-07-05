398f4e8be304f1663739aba4b3dc0846d4265503
Tony Wildish <Tony.Wildish@cern.ch>
2013-07-05 16:10:42 +0200

    test for TESTBED_ROOT being defined before launching scripts

--- PhEDEx-central-agents/stop-agents.sh
+++ PhEDEx-central-agents/stop-agents.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -d "$TESTBED_ROOT" ]; then
+  echo "TESTBED_ROOT not defined or not a directory"
+  exit 0
+fi
+
 . $TESTBED_ROOT/env-anse.sh
 $PHEDEX_ROOT/Utilities/Master --config Config.Mgmt.Testbed stop
 for dir in $TESTBED_ROOT/Testbed2_Mgmt/state/*/
--- PhEDEx-site-agents/start-agents.sh
+++ PhEDEx-site-agents/start-agents.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -d "$TESTBED_ROOT" ]; then
+  echo "TESTBED_ROOT not defined or not a directory"
+  exit 0
+fi
+
 . $TESTBED_ROOT/env-anse.sh
 $PHEDEX_ROOT/Utilities/Master --config Config.T2_Test1_Buffer start watchdog
 $PHEDEX_ROOT/Utilities/Master --config Config.T2_Test2_Buffer start watchdog
