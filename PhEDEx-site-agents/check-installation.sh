b1c3923ec93d6bfbb656cbd57ab6922e605e31df
Tony Wildish <Tony.Wildish@cern.ch>
2013-07-23 12:08:46 +0200

    first pass at checking installation

--- PhEDEx-site-agents/check-installation.sh
+++ PhEDEx-site-agents/check-installation.sh
@@ -1,7 +1,50 @@
 #!/bin/bash
 
-# Check lifecycle agent works (not needed?)
+cd "`dirname $0`"
+
+# Check the environment
+echo "Checking your environment"
+if [ -d "$TESTBED_ROOT" ]; then
+  echo "TESTBED_ROOT set, good..."
+else
+  echo "TESTBED_ROOT not defined or not a directory"
+  exit 0
+fi
+
+if [ ! -f . $TESTBED_ROOT/env-anse.sh ]; then
+  echo "Cannot find your \$TESTBED_ROOT/env-anse.sh file, did your installation work?"
+  exit 0
+fi
+. $TESTBED_ROOT/env-anse.sh
+
+if [ -z "$PHEDEX_SITE" ]; then
+  echo "PHEDEX_SITE set to $PHEDEX_SITE, good..."
+else
+  echo "PHEDEX_SITE not defined. Did you set it in \$TESTBED_ROOT/env-anse.sh?"
+  exit 0
+fi
+
+if [ `echo $PHEDEX_SITE | grep -c Buffer` -gt 0 ]; then
+  echo "Your PHEDEX_SITE variable should not have the '_Buffer' extension"
+  exit 0
+fi
+if [ "$PHEDEX_SITE" -eq "InsertYourSiteNameHere" ]; then
+  echo "You have not defined the PHEDEX_SITE variable yet."
+  echo "You need to set it in \$TESTBED_ROOT/env-anse.sh"
+  exit 0
+fi
+
+# Check the user has a DBParam
+param=`echo $PHEDEX_DBPARAM | awk -F: '{ print $1 }'`
+if [ -f $param ]; then
+  echo "You have a DBParam file at $param, good..."
+else
+  echo "You do not have a DBParam file yet, it should be at $param"
+  exit 0
+fi
 
 # Check PhEDEx DB connection works
+echo "Checking your database connection"
+$PHEDEX_ROOT/Utilities/Master --config Config.ANSE checkdb
 
-# Check Configuration files are set up correctly
+# Check lifecycle agent works? (not needed...)
