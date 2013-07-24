f551aa6f4d3d2ee1e99e261695682ca507ab4b26
Tony Wildish <Tony.Wildish@cern.ch>
2013-07-24 18:33:05 +0200

    add libwww-perl to the mix

--- VagrantUp/install-system.sh
+++ VagrantUp/install-system.sh
@@ -7,4 +7,4 @@ echo "Checking your system has the necessary bits"
 # Seeds taken from /tmp/tmp*/slc5_amd64_gcc461-driver.txt
 slc5_amd64_platformSeeds="glibc coreutils bash tcsh zsh perl tcl tk readline openssl ncurses e2fsprogs krb5-libs freetype fontconfig compat-libstdc++-33 libidn libX11 libXmu libSM libICE libXcursor libXext libXrandr libXft mesa-libGLU mesa-libGL e2fsprogs-libs libXi libXinerama libXft libXrender libXpm"
 
-sudo yum install -y git zsh rlwrap $slc5_amd64_platformSeeds
+sudo yum install -y git zsh rlwrap perl-libwww-perl-5.805 $slc5_amd64_platformSeeds
