760de806f345d8c42c3b14656d15bab4dfe906a9
Tony Wildish <Tony.Wildish@cern.ch>
2013-07-25 12:23:48 +0200

    working version

--- PhEDEx-admin/developer-setup.sh
+++ PhEDEx-admin/developer-setup.sh
@@ -8,26 +8,26 @@ gitsrc=git@github.com:TonyWildish
 
 . $TESTBED_ROOT/env-anse.sh
 
-#echo "Install development path for LifeCycle agent"
-#cd $TESTBED_ROOT/sw/$SCRAM_ARCH/cms/PHEDEX-lifecycle/*
-#rpm_installed=`pwd`
-#cd ..
-#git clone -b PHEDEX-lifecycle $gitsrc/PHEDEX.git LifeCycle-git
-#cd $rpm_installed
-#mkdir rpm_orig
-#for dir in Testbed Utilities perl_lib
-#do
-#  mv $dir rpm_orig/
-#  ln -s ../LifeCycle-git/$dir $dir
-#done
-#
-#cd bin
-#mkdir rpm_orig
-#for file in *
-#do
-#  mv $file rpm_orig/
-#  ln -s `find ../../LifeCycle-git/ -name $file` .
-#done
+echo "Install development path for LifeCycle agent"
+cd $TESTBED_ROOT/sw/$SCRAM_ARCH/cms/PHEDEX-lifecycle/*
+rpm_installed=`pwd`
+cd ..
+git clone -b PHEDEX-lifecycle $gitsrc/PHEDEX.git LifeCycle-git
+cd $rpm_installed
+mkdir rpm_orig
+for dir in Testbed Utilities perl_lib
+do
+  mv $dir rpm_orig/
+  ln -s ../LifeCycle-git/$dir $dir
+done
+
+cd bin
+mkdir rpm_orig
+for file in *
+do
+  mv $file rpm_orig/
+  ln -s `find ../../LifeCycle-git/ -name $file` .
+done
 
 echo "Install development path for PhEDEx agents"
 cd $TESTBED_ROOT/sw/$SCRAM_ARCH/cms/PHEDEX-admin/*
