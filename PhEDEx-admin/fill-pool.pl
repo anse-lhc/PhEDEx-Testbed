fbb4ade2f03760c76f54f9895b56a19c8585a12b
Tony Wildish <Tony.Wildish@cern.ch>
2013-08-02 10:27:22 +0200

    add statistice to fill-pool.pl if it dies for some reason.
    add Injection.conf, to inject data at a site and also create
    soft-links or hard-links to populate a storage element with
    pseudo-real files.
    
    As configured, Injection.conf will inject 627,500 files in one
    dataset, in blocks of 2500 files each. If the files are linked
    to a source file of 2 GB, that corresponds to 1.3 PB of data.
    
    Change the injection node and the dataset name if you want to run this
    locally!

--- PhEDEx-admin/fill-pool.pl
+++ PhEDEx-admin/fill-pool.pl
@@ -9,9 +9,9 @@ use POSIX;
 use File::Path;
 
 my ($help,$verbose,$debug,$file,$dir,$i,$j,$k,$numFiles,$perLevel,$len,$dst);
-my ($result,$numDirs,$err);
+my ($result,$numDirs,$err,$symlink,$step,$total);
 
-$help = $verbose = $debug = $err = 0;
+$help = $verbose = $debug = $err = $symlink = 0;
 $numFiles = 10000;
 
 GetOptions(
@@ -19,6 +19,7 @@ GetOptions(
 	    'file=s'	=> \$file,
 	    'num=i'	=> \$numFiles,
 	    'help'	=> \$help,
+	    'symlink'	=> \$symlink,
 	    'debug'	=> \$debug,
 	    'verbose'	=> \$verbose,
 	  );
@@ -34,20 +35,25 @@ $numFiles > 0 or die "Number of files (--num) must be positive\n";
 if ( $numFiles > 256**3 ) {
   die "Cannot create more than 256^3 files, sorry!\n(That's about 16 million files)\n";
 }
-# In fact there's a harder limit than that...
-die "Cannot create more than 32K files in one go, that seems to be a limit of ext2fs\n"
-  if $numFiles > 32000;
+if ( ! $symlink ) {
+# There's a harder limit than that for hard links...
+  die "Cannot create more than 32K files in one go (a filesystem limit)\n"
+    if $numFiles > 32000;
+}
 
 $perLevel = 256;
 $len      = 2;
-print "Creating $numFiles files in $dir.\n";
+print "Creating $numFiles files in $dst\n";
 print "Using $perLevel files per directory\n";
 
 $i = ceil( log($numFiles)/log(256) );
 $i++ if $i%2;
 $numDirs = ceil($numFiles/256);
+$total = $numFiles;
 
+$|=1;
 while ( $numDirs > 0 ) {
+  print "$numDirs directories to go...  \r" unless $numDirs %100;
   $dir = sprintf("%0${i}x",$numDirs);
   $dir = join('/',unpack("(A$len)*",$dir));
   $verbose && print 'Create directory ',$dir,"\n";
@@ -56,14 +62,16 @@ while ( $numDirs > 0 ) {
   for ($j = 255; $j>0; $j--) {
     $result = $dir . '/file-' . sprintf('%02x',$j) . '.dat';
     $debug && print "Link $file to $result\n";
-    link $file,$result or do {
-      warn "Cannot link $file to $result\n";
-      $err++;
-      die "Too many errors ($err)\n" if $err >= 10;
-    };
+    if ( $symlink ) {
+      symlink $file,$result or die "\nCannot symlink $file to $result (",$total-$numFiles," links created)\n";
+    } else {
+      link $file,$result or die "\nCannot link $file to $result (",$total-$numFiles," links created)\n";
+    }
   }
   $numDirs--;
+  $numFiles--;
 }
+  print "                       \r";
 
 print "All done\n";
 
