34abb03a702f1cd5bdd22e5ffbfe20afd17c8b6f
Vlad Lapadatescu <vlad@cern.ch>
2014-10-24 13:40:17 +0200

    FIXED: wrong link to script

--- VagrantUp/install-phedex-boot.sh
+++ VagrantUp/install-phedex-boot.sh
@@ -2,5 +2,5 @@
 
 log=/vagrant/.bootstrap/phedex.log
 echo Installing PhEDEx site agents. This can take 5-10 minutes
-sudo -i -u vagrant /vagrant/install-phedex.sh 2>&1 | tee $log >/dev/null
+sudo -i -u vagrant /vagrant/install-phedex-el5.sh 2>&1 | tee $log >/dev/null
 chown vagrant.vagrant $log
